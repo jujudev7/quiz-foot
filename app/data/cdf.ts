a742cb019d3f9689a48d7da711c88ebfea2d4ef7
jujudev7 <penna.julien@gmail.com>
2024-09-30 15:58:45 +0200

    Quiz Cdf added

---
+++ app/data/cdf.ts
@@ -0,0 +1,103 @@
+// app/data/questions.ts
+const questions = [
+  {
+    question: "Quel club a remporté le plus de Coupes de France ?",
+    options: [
+      "Paris Saint-Germain",
+      "AS Saint-Étienne",
+      "Olympique de Marseille",
+      "RC Lens",
+    ],
+    correctAnswer: "Paris Saint-Germain",
+    explanation:
+      "Le PSG est le recordman de victoires en Coupe de France avec 15 trophées à son actif. L'OM en a remporté 10, Saint-Étienne 6, et étonnement, le RC Lens n'en a encore jamais gagné ! ",
+  },
+  {
+    question: "Quel est le nom de l'organisme qui gère la Coupe de France ?",
+    options: ["LFP", "FFF", "UEFA", "FIFA"],
+    correctAnswer: "FFF",
+    explanation:
+      "C'est la FFF (Fédération Française de Football) qui organise chaque année la Coupe de France. La LFP (Ligue de Football Professionnel), quant à elle, gère le championnat de France. L'UEFA est l'instance qui gère le football à l'échelle européenne (Euro et Coupes d'Europe), tandis que la FIFA gère le football international, notamment la Coupe du monde.",
+  },
+  {
+    question:
+      "En quelle année s'est tenue la première finale de la Coupe de France ?",
+    options: ["1898", "1908", "1918", "1928"],
+    correctAnswer: "1918",
+    explanation:
+      "La première édition de la Coupe de France a eu lieu en 1917/1918, pendant la Première Guerre mondiale en pleine Union sacrée. Elle a été remportée par l'Olympique, aussi surnommé l'Olympique de Pantin ou l'Olympique de Paris.",
+  },
+  {
+    question:
+      "Quelle est la particularité de la Coupe de France par rapport aux autres compétitions nationales ?",
+    options: [
+      "Elle est interdite aux mineurs.",
+      "Elle est ouverte aux clubs amateurs.",
+      "Elle est réservée uniquement aux clubs professionnels.",
+      "Elle ne se joue que le dimanche.",
+    ],
+    correctAnswer: "Elle est ouverte aux clubs amateurs.",
+    explanation:
+      "La Coupe de France se distingue par son ouverture à la fois aux clubs amateurs et professionnels, permettant à de petites équipes de se mesurer à des grands clubs.",
+  },
+  {
+    question:
+      "Combien d'équipes de deuxième division ont remporté la Coupe de France ?",
+    options: ["0", "2", "5", "10"],
+    correctAnswer: "2",
+    explanation:
+      "Seules 2 équipes de D2 ont réussit l'exploit de remporter la Coupe de France : Le Havre AC en 1959 (2-2 a.p. puis match rejoué et gagné 3-0) et l'En Avant de Guingamp (2-1 contre Rennes en 2009).",
+  },
+  {
+    question:
+      "Quel club a été le premier à remporter 3 fois de suite la Coupe de France ?",
+    options: [
+      "Red Star",
+      "Olympique de Marseille",
+      "LOSC Lille",
+      "Paris Saint-Germain",
+    ],
+    correctAnswer: "Red Star",
+    explanation:
+      "Le Red Star a été la première équipe à signer un \"triplé\" en Coupe de France, entre 1921 et 1923. Dans la foulée, l'OM a remporté 3 fois le trophées, mais en 4 ans. Lille a également conquis 3 Coupes nationales d'affilée entre 1946 et 1948. Enfin, le PSG est la 1ère équipe à avoir gagné 4 Coupes de France consécutives (2015-2018).",
+  },
+  {
+    question:
+      "Actuellement, combien de clubs participent à la Coupe de France ?",
+    options: ["200", "1000", "4000", "7000"],
+    correctAnswer: "7000",
+    explanation:
+      "Environ 7 000 clubs participent désormais à la Coupe de France, incluant des clubs amateurs et professionnels. Lors de la 1ère édition (1917/1918), il n'y avait que 48 équipes !",
+  },
+  {
+    question:
+      "En 31 matchs de Coupe de France, combien de buts a inscrit Kylian Mbappé ?",
+    options: ["11", "17", "31", "37"],
+    correctAnswer: "37",
+    explanation:
+      "Kylian Mbappé a inscrit la bagatelle de 37 buts en 31 rencontres de Coupe de France, auxquels il faut aussi ajouter 15 passes décisives !",
+  },
+  {
+    question:
+      "Quelle est la particularité du parcours victorieux en Coupe de France du FC Metz en 1987/1988 ?",
+    options: [
+      "L'équipe était composée à 100% de joueurs étrangers",
+      "Le club n'a pas encaissé le moindre but",
+      "Le club n'a pas rencontré une seule équipe de D1",
+      "L'équipe a reçu un carton rouge à chaque match",
+    ],
+    correctAnswer: "Le club n'a pas rencontré une seule équipe de D1",
+    explanation:
+      "Vainqueur en finale de Sochaux (1-1 a.p., 5-4 t.a.b), le FC Metz n'a pas affronté la moindre équipe de Division 1 durant tout son parcours !",
+  },
+  {
+    question:
+      "Combien de clubs d'Outre-mer (Guyane, Guadeloupe, Martinique, Réunion, Polynésie, Nouvelle-Calédonie, Mayotte) ont participé à l'édition 2023/2024 de la Coupe de France ?",
+    options: ["8", "42", "203", "341"],
+    correctAnswer: "341",
+    explanation:
+      "341 clubs d'Outre-mer ont participé à la Coupe de France 2023/2024. Ils étaient 42 lors de la saison 1970/1971 et 203 en 2001/2002.",
+  },
+];
+
+export default questions;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // app/page.tsx
+import QuizCdf from "./components/QuizCdf";
 import QuizL1 from "./components/QuizL1";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="container mx-auto w-full p-4 flex flex-col items-center">
       <h1 className="text-2xl font-bold mb-4">Quiz React avec Next.js</h1>
       <QuizL1 />
+      <QuizCdf />
     </div>
   );
 }
